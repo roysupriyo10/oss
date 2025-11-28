7572c874af8a63a2f54bf41849fad5fd60595662
Supriyo Roy <roysupriyo10@gmail.com>
2025-11-28 09:47:14 +0530

    chore: first commit

---
+++ eslint.config.ts
@@ -0,0 +1,11 @@
+import jseslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  {
+    ignores: ["**/dist/**", "**/node_modules/**"],
+  },
+  jseslint.configs.recommended,
+  tseslint.configs.recommended,
+);
